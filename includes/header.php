abfb60b909a232a36be4d3bf958692c79d93b5d1
Faizkhan710 <illahkhanfaiz@gmail.com>
2023-02-19 11:30:05 -0800

    movies

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<div class="navbar-brand">
+    <a href="index.php">
+        <h1 class="navbar-heading">ARVR Cinema</h1>
+    </a>
+</div>
+<div class="navbar-container">
+    <nav class="navbar">
+        <ul class="navbar-menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="schedule.php">Schedule</a></li>
+            <!-- <li><a href="TxnStatus.php" target="_blank">Status</a></li> -->
+            <li><a href="#">Team</a></li>           
+            <li><a href="contact-us.php">Contact</a></li>
+        </ul>
+    </nav>
+</div>
